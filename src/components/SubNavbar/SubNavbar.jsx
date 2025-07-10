b03b621a58a791b2e39f35da0652bc1920322b17
gopi-c1 <Tayanoorugopi@sltechnocrats.net>
2025-07-10 11:51:50 +0530

    "prec"

--- src/components/SubNavbar/SubNavbar.jsx
+++ src/components/SubNavbar/SubNavbar.jsx
@@ -8,7 +8,7 @@ const SubNavbar = () => {
         <li><a href="/AllMedicinesPage">Order Medicine</a></li>
         <li><a href="/ProductsPage"></a>More Product</li>
         <li><a href="/MedicalStoresPage">Medical stores</a></li>
-        <li> <a href="/PrescriptionPage">Order with Prescription</a></li>
+        <li> <a href="/PrescriptionPage">Order with</a></li>
         <li><a href='/DoctorAppointmentsPage'>Doctor Consultation</a></li>
         <li><a href="/Labs">Labs</a></li>
         <li><a href="/BloodPage">Blood Bank</a></li>
